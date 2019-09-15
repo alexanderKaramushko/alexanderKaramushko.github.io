a2a7591e3f98657be8fe7f510d0605731ab798c5
Alexander <a.morgoth.b@gmail.com>
2019-09-15 12:27:42 +0300

    created new event listeners and added fixed anchor scroll

--- js/es5/script.js
+++ js/es5/script.js
@@ -1,7 +1,17 @@
 "use strict";
 
-$(document).ready(function() {
-  $(function() {
+var lazy = [];
+
+function registerListener (event, func) {
+  if (window.addEventListener) {
+    window.addEventListener(event, func);
+  } else {
+    window.attachEvent("on" + event, func);
+  }
+}
+
+function initTooltip () {
+  $(function () {
     $('[data-toggle="tooltip"]').tooltip({
       delay: {
         show: 200,
@@ -9,35 +19,28 @@ $(document).ready(function() {
       }
     });
   });
+}
 
-  window.onscroll = function() {
+function initScrollableHeader () {
+  window.onscroll = function () {
     if ($(document).scrollTop() > 80) {
       $("header").addClass("header_scrolled");
     } else {
       $("header").removeClass("header_scrolled");
     }
   };
+}
 
-  objectFitImages();
-});
-
-var lazy = [];
-
-registerListener("load", setLazy);
-registerListener("load", lazyLoad);
-registerListener("scroll", lazyLoad);
-registerListener("resize", lazyLoad);
-
-function setLazy() {
+function setLazy () {
   lazy = document.getElementsByClassName("lazy");
 }
 
-function lazyLoad() {
+function lazyLoad () {
   for (var i = 0; i < lazy.length; i++) {
     if (isInViewport(lazy[i])) {
       if (lazy[i].getAttribute("data-src")) {
         lazy[i].src = lazy[i].getAttribute("data-src");
-        lazy[i].onload = function() {
+        lazy[i].onload = function () {
           this.classList.add("img-loaded-js");
         };
         lazy[i].removeAttribute("data-src");
@@ -48,13 +51,13 @@ function lazyLoad() {
   cleanLazy();
 }
 
-function cleanLazy() {
-  lazy = Array.prototype.filter.call(lazy, function(l) {
+function cleanLazy () {
+  lazy = Array.prototype.filter.call(lazy, function (l) {
     return l.getAttribute("data-src");
   });
 }
 
-function isInViewport(el) {
+function isInViewport (el) {
   var rect = el.getBoundingClientRect();
 
   return (
@@ -65,10 +68,90 @@ function isInViewport(el) {
   );
 }
 
-function registerListener(event, func) {
-  if (window.addEventListener) {
-    window.addEventListener(event, func);
-  } else {
-    window.attachEvent("on" + event, func);
-  }
-}
+registerListener("load", setLazy);
+registerListener("load", lazyLoad);
+registerListener("load", initTooltip);
+registerListener("load", initScrollableHeader);
+registerListener("load", objectFitImages);
+registerListener("scroll", lazyLoad);
+registerListener("resize", lazyLoad);
+
+(function (document, history, location) {
+  var HISTORY_SUPPORT = !!(history && history.pushState);
+
+  var anchorScrolls = {
+    ANCHOR_REGEX: /^#[^ ]+$/,
+    OFFSET_HEIGHT_PX: document.querySelector('.fixed-header').getBoundingClientRect().height,
+
+    /**
+     * Establish events, and fix initial scroll position if a hash is provided.
+     */
+    init: function () {
+      this.scrollToCurrent();
+      window.addEventListener('hashchange', this.scrollToCurrent.bind(this));
+      document.body.addEventListener('click', this.delegateAnchors.bind(this));
+    },
+
+    /**
+     * Return the offset amount to deduct from the normal scroll position.
+     * Modify as appropriate to allow for dynamic calculations
+     */
+    getFixedOffset: function () {
+      return this.OFFSET_HEIGHT_PX;
+    },
+
+    /**
+     * If the provided href is an anchor which resolves to an element on the
+     * page, scroll to it.
+     * @param  {String} href
+     * @return {Boolean} - Was the href an anchor.
+     */
+    scrollIfAnchor: function (href, pushToHistory) {
+      var match, rect, anchorOffset;
+
+      if (!this.ANCHOR_REGEX.test(href)) {
+        return false;
+      }
+
+      match = document.getElementById(href.slice(1));
+
+      if (match) {
+        rect = match.getBoundingClientRect();
+        anchorOffset = window.pageYOffset + rect.top - this.getFixedOffset();
+        window.scrollTo(window.pageXOffset, anchorOffset);
+
+        // Add the state to history as-per normal anchor links
+        if (HISTORY_SUPPORT && pushToHistory) {
+          history.pushState({}, document.title, location.pathname + href);
+        }
+      }
+
+      return !!match;
+    },
+
+    /**
+     * Attempt to scroll to the current location's hash.
+     */
+    scrollToCurrent: function () {
+      this.scrollIfAnchor(window.location.hash);
+    },
+
+    /**
+     * If the click event's target was an anchor, fix the scroll position.
+     */
+    delegateAnchors: function (e) {
+      var elem = e.target;
+
+      if (
+        elem.nodeName === 'A' &&
+        this.scrollIfAnchor(elem.getAttribute('href'), true)
+      ) {
+        e.preventDefault();
+      }
+    }
+  };
+
+  window.addEventListener(
+    'DOMContentLoaded', anchorScrolls.init.bind(anchorScrolls)
+  );
+})(window.document, window.history, window.location);
